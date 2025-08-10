f5d1c522cf6c39ab30fa6cdef258d75a85ea9560
Tushar Reddy <reddytushar295@outlook.com>
2025-08-10 22:09:10 +0530

    TF format fixed

--- lambda.tf
+++ lambda.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
-resource "aws_lambda_function" "test1"{
-    filename = "${path.module}/Function.zip"
-    function_name = "test1"
-    handler = "Lambda.lambda_handler"
-    runtime = "python3.12"  
-    role="arn:aws:iam::268345526554:role/y1"
-    source_code_hash = filebase64sha256("${path.module}/Function.zip")
+resource "aws_lambda_function" "test1" {
+  filename         = "${path.module}/Function.zip"
+  function_name    = "test1"
+  handler          = "Lambda.lambda_handler"
+  runtime          = "python3.12"
+  role             = "arn:aws:iam::268345526554:role/y1"
+  source_code_hash = filebase64sha256("${path.module}/Function.zip")
 
 }
