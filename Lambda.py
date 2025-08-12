e21a6bb663979b15ff60d65b8dee8003e491500b
Tushar Reddy <reddytushar295@outlook.com>
2025-08-12 21:25:34 +0530

    Updated Lambda

--- Lambda.py
+++ Lambda.py
@@ -2,8 +2,8 @@ import json
 
 def lambda_handler(event, context):
     # Sample Lambda function handler
-    print("Hello from Lambda!")
+    print("Hello from Lambda2!")
     return{
         'statusCode': 200,
-        'body': 'Hello from Lambda!'
+        'body': 'Hello from Lambda2!'
     }
