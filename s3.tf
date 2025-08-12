37e04b6e0ada62a677f47b21f17e5fa5088bf6a8
Tushar Reddy <reddytushar295@outlook.com>
2025-08-12 21:57:37 +0530

    S3 folder Created

--- s3.tf
+++ s3.tf
@@ -3,3 +3,17 @@ resource "aws_s3_bucket" "my-bucket" {
   acl    = "private"
 
 }
+
+resource "aws_s3_bucket_object" "folder1" {
+  bucket  = aws_s3_bucket.my-bucket.bucket
+  key     = "uploads/"
+  acl     = "private"
+  content = ""
+}
+
+resource "aws_s3_bucket_object" "folder2" {
+  bucket  = aws_s3_bucket.my-bucket.bucket
+  key     = "processed/"
+  acl     = "private"
+  content = ""
+}
