6667d8adea9ab9a1f46c0d5721636279b5b4dcf2
Tushar Reddy <reddytushar295@outlook.com>
2025-08-12 21:51:05 +0530

    S3 Added

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
-resource "aws_s3_bucket" "my-bucket"{
-    bucket="image-processing-124"
-    acl="private"
-    
+resource "aws_s3_bucket" "my-bucket" {
+  bucket = "image-processing-124"
+  acl    = "private"
+
 }
